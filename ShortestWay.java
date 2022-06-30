3676d95cd069aeea56725de3e36d3786f2c0c0e8
skatkar <skatkar225@gmail.com>
2022-06-30 16:34:04 -0500

    Greedy-4 submission

--- ShortestWay.java
+++ ShortestWay.java
@@ -1,7 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ShortestWay {
+    // Linear search
     // TC : O(m * n)
     // SC : O(1)
     public int shortestWay(String source, String target) {
@@ -33,6 +33,44 @@ public class ShortestWay {
             }
         }
 
+        return result;
+    }
+
+    // TC : O(m* log n) m- length of target string, n-length of source string
+    // SC : O(1)
+    public int shortestWay_binarySearch(String source, String target) {
+        if(source == null || source.length() == 0) return 0;
+
+        // This will help us if we encounter a character in target which is not there in source
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for(int i=0; i < source.length();i ++) {
+            char c = source.charAt(i);
+            if(!map.containsKey(c)) map.put(c, new ArrayList<>());
+            map.get(c).add(i);
+        }
+
+        int i=0; // pointer in target
+        int pos=0; // pointer in source
+        int tl = target.length();
+        int sl = source.length();
+        int result = 1; //assuming there will be at least 1 subsequence
+
+        while(i < tl) {
+            char c = target.charAt(i);
+            if(!map.containsKey(c)) return -1;
+
+            List<Integer> list = map.get(c);
+            int k = Collections.binarySearch(list, pos);
+
+            if(k < 0) k = -k-1;
+            if(k < list.size()){
+                pos = 0;
+                result++;
+            }else {
+                pos = list.get(k) + 1;
+                i++;
+            }
+        }
         return result;
     }
 }
